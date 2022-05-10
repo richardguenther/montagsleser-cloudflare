aa718a810bebe6a1382127ac1027196d48777bd2
richard <richard@uni-bremen.de>
2022-05-10 21:19:34 +0000

    gut umgezogen

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,7 +1,7 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 async function getWpPosts() {
-  const url = "https://acf.montagsleser.de/wp-json/wp/v2/posts?_embed&per_page=6";
+  const url = "https://inhalt.montagsleser.de/wp-json/wp/v2/kunstwerk?per_page=12";
 
   const response = EleventyFetch(url, {
     dureation: "1d",
