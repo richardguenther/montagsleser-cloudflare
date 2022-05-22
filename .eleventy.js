54bdcc8467c8844afd41c432daeb8049635557d7
richard <richard@uni-bremen.de>
2022-05-22 15:54:13 +0000

    150er bilder

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ const Image = require("@11ty/eleventy-img");
 
 async function imageShortcode(src, cls, alt, sizes) {
   let metadata = await Image(src, {
-    widths: [300, 600],
+    widths: [150, 300, 600],
     formats: ["avif", "jpeg"],
     outputDir: "_fetched/imagesOptimized",
     urlPath: "/img/"
